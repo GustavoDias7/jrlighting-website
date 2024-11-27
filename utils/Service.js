3493474b44d86453fd4d0bfd04bf08134aa4c83b
Gustavo <gustavo7dias@gmail.com>
2024-11-27 12:34:45 -0300

    Update service page, create other_services layout and add whatsapp link to the Service class

--- utils/Service.js
+++ utils/Service.js
@@ -1,25 +1,36 @@
 class Service {
   constructor(props = {}) {
-    const { id, name, description, img } = props;
+    const { id, name, description, img, whatsapp } = props;
     this.id = id;
     this.name = name;
     this.description = description;
     this.img = img;
+    this.whatsapp = whatsapp;
 
-    this.link = this.get_link();
-    this.short_description = this.get_short_description();
+    this.link = this.set_link();
+    this.short_description = this.set_short_description();
+    this.whatsapp_link = this.set_whatsapp_link();
   }
 
-  get_link() {
+  set_link() {
     const url = new URLSearchParams();
     url.set("id", this.id);
     return "/service?" + url.toString();
   }
 
-  get_short_description() {
+  set_short_description() {
     const len = this.description.length;
     const short_descript = this.description.slice(0, len > 55 ? 55 : len - 1);
     return short_descript + "...";
+  }
+
+  set_whatsapp_link() {
+    const url = new URLSearchParams();
+    url.set(
+      "text",
+      `Olá, eu gostaria de fazer o pedido do serviço: ${this.name}.`
+    );
+    return `${this.whatsapp}?${url.toString()}`;
   }
 }
 
--- utils/constantes.js
+++ utils/constantes.js
@@ -9,25 +9,25 @@ const context = {
   faq: [
     {
       id: 0,
-      question: "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+      question: "Lorem ipsum dolor sit amet consectetur adipisicing elit? 0",
       response:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
     },
     {
       id: 1,
-      question: "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+      question: "Lorem ipsum dolor sit amet consectetur adipisicing elit? 1",
       response:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
     },
     {
       id: 2,
-      question: "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+      question: "Lorem ipsum dolor sit amet consectetur adipisicing elit? 2",
       response:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
     },
     {
       id: 3,
-      question: "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+      question: "Lorem ipsum dolor sit amet consectetur adipisicing elit? 3",
       response:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
     },
@@ -70,53 +70,60 @@ const context = {
     email: "email@email.com",
     maps: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d470400.7386830834!2d-43.77565508292252!3d-22.91315798611381!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bde559108a05b%3A0x50dc426c672fd24e!2sRio%20de%20Janeiro%2C%20State%20of%20Rio%20de%20Janeiro!5e0!3m2!1sen!2sbr!4v1725457331794!5m2!1sen!2sbr",
   }),
-  services: [
-    new Service({
-      id: 0,
-      name: "Lorem ipsum dolor",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
-      img: "../../img/placeholder-1.svg",
-    }),
-    new Service({
-      id: 1,
-      name: "Lorem ipsum dolor",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
-      img: "../../img/placeholder-2.svg",
-    }),
-    new Service({
-      id: 2,
-      name: "Lorem ipsum dolor",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
-      img: "../../img/placeholder-1.svg",
-    }),
-    new Service({
-      id: 0,
-      name: "Lorem ipsum dolor",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
-      img: "../../img/placeholder-1.svg",
-    }),
-    new Service({
-      id: 1,
-      name: "Lorem ipsum dolor",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
-      img: "../../img/placeholder-2.svg",
-    }),
-    new Service({
-      id: 2,
-      name: "Lorem ipsum dolor",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
-      img: "../../img/placeholder-1.svg",
-    }),
-  ],
+  services: [],
   getServices() {
     return JSON.stringify(this.ctx.services);
   },
 };
+context.services.push(
+  new Service({
+    id: 0,
+    name: "Lorem ipsum dolor 0",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
+    img: "../../img/placeholder-1.svg",
+    whatsapp: context.social_links.whatsapp,
+  }),
+  new Service({
+    id: 1,
+    name: "Lorem ipsum dolor 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
+    img: "../../img/placeholder-2.svg",
+    whatsapp: context.social_links.whatsapp,
+  }),
+  new Service({
+    id: 2,
+    name: "Lorem ipsum dolor 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
+    img: "../../img/placeholder-1.svg",
+    whatsapp: context.social_links.whatsapp,
+  }),
+  new Service({
+    id: 3,
+    name: "Lorem ipsum dolor 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
+    img: "../../img/placeholder-1.svg",
+    whatsapp: context.social_links.whatsapp,
+  }),
+  new Service({
+    id: 4,
+    name: "Lorem ipsum dolor 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
+    img: "../../img/placeholder-2.svg",
+    whatsapp: context.social_links.whatsapp,
+  }),
+  new Service({
+    id: 5,
+    name: "Lorem ipsum dolor 5",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab veniam qui ducimus nesciunt cupiditate, ullam suscipit autem. Perspiciatis at ipsam velit pariatur quae. Voluptatem sequi quibusdam minima quae laborum.",
+    img: "../../img/placeholder-1.svg",
+    whatsapp: context.social_links.whatsapp,
+  })
+);
 
 module.exports = { context };
